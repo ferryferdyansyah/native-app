76fc9fd162fcd1c2efcbc1d7f032c9921dbef4db
ferryferdyansyah <ferryferdyansyah002@gmail.com>
2023-11-03 20:01:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './navigation/Bottomtabs';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
